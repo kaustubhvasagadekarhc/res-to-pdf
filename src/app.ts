3d4498f503dcbacfefbbba0780a134d58facdf0a
kaustubhvasagadekarhc <kaustubh.vasagadekar@humancloud.co.in>
2025-12-18 10:42:39 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS configuration for cookie-based authentication
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', "https://res-to-pdf.vercel.app"] ,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
